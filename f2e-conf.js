495806f9ac13272e58bfb4478352080a6da640ec
shiyangyang <shy2850@163.com>
2017-05-06 18:20:36 +0800

    init f2e-conf.js

---
+++ f2e-conf.js
@@ -0,0 +1,43 @@
+var path = require('path');
+var output_path = path.join(__dirname, '../flow-portal-out');
+var dev_port = 8012;
+
+exports["localhost"] = {
+	root: __dirname,
+	port: dev_port,
+	buildFilter: function(path){
+        var regs = [
+        	/\/(js|css)(\/index\..*)?$/,
+        	/\/img(\/.*)?/,
+        	/\/(index\.html)?$/
+        ];
+
+        for (var i = 0; i < regs.length; i++) {
+        	if (regs[i].test(path)) {
+        		return true;
+        	}
+        }
+        return false;
+    },
+    agent: {
+    	get: function (pathname) {
+    		if (pathname.match(/\.css$/)) {
+    			return {
+    				host: 'localhost',
+    				port: dev_port,
+    				path: function () {return pathname.replace(/\.css$/, '.less')}
+    			}
+    		}
+    	}
+    },
+	output: output_path
+};
+
+exports["output_path"] = {
+	root: output_path,
+	welcome: 'index.html',
+	port: 8013,
+	runJs: false,
+	gzip: true,
+	cdn: true
+}
---
+++ js/index.js
@@ -0,0 +1,7 @@
+$include["../node_modules/jquery/dist/jquery.js"];
+$include["../node_modules/fullpage.js/dist/jquery.fullpage.js"];
+(function ($) {
+	$(function () {
+		$('#flowpp-container').fullpage();
+	});
+})(jQuery);
