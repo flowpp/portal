f5e95b54e36b9df8f7cb95faa4439c76e5a906c5
shy2850 <shy2850.com>
2017-05-07 20:44:55 +0800

    header begin

--- f2e-conf.js
+++ f2e-conf.js
@@ -3,41 +3,41 @@ var output_path = path.join(__dirname, '../flow-portal-out');
 var dev_port = 8012;
 
 exports["localhost"] = {
-	root: __dirname,
-	port: dev_port,
-	buildFilter: function(path){
+    root: __dirname,
+    port: dev_port,
+    buildFilter: function(path){
         var regs = [
-        	/\/(js|css)(\/index\..*)?$/,
-        	/\/img(\/.*)?/,
-        	/\/(index\.html)?$/
+            /\/(js|css)(\/index\..*)?($|\?.*)/,
+            /\/img(\/.*)?/,
+            /\/(index\.html|favicon\.ico)?($|\?.*)/
         ];
 
         for (var i = 0; i < regs.length; i++) {
-        	if (regs[i].test(path)) {
-        		return true;
-        	}
+            if (regs[i].test(path)) {
+                return true;
+            }
         }
         return false;
     },
     agent: {
-    	get: function (pathname) {
-    		if (pathname.match(/\.css$/)) {
-    			return {
-    				host: 'localhost',
-    				port: dev_port,
-    				path: function () {return pathname.replace(/\.css$/, '.less')}
-    			}
-    		}
-    	}
+        get: function (pathname) {
+            if (pathname.match(/\.css$/)) {
+                return {
+                    host: 'localhost',
+                    port: dev_port,
+                    path: function () {return pathname.replace(/\.css$/, '.less')}
+                }
+            }
+        }
     },
-	output: output_path
+    output: output_path
 };
 
 exports["output_path"] = {
-	root: output_path,
-	welcome: 'index.html',
-	port: 8013,
-	runJs: false,
-	gzip: true,
-	cdn: true
+    root: output_path,
+    welcome: 'index.html',
+    port: 8013,
+    runJs: false,
+    gzip: true,
+    cdn: true
 }
--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 $include["../node_modules/jquery/dist/jquery.js"];
+$include["../node_modules/bootstrap/dist/js/bootstrap.js"];
 $include["../node_modules/fullpage.js/dist/jquery.fullpage.js"];
 (function ($) {
-	$(function () {
-		$('#flowpp-container').fullpage();
+	$(document).ready(function () {
+		var container = $('#flowpp-container');
+		container.fullpage({
+			anchors: ['flow-index', 'flow-flowpp', 'flow-flowai', 'flow-about'],
+			menu: '#flow-menu'
+		});
 	});
 })(jQuery);
